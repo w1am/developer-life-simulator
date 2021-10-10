2985cc18f156dd50a09fe2b5622d926807b37668
w1am <william@inorbit.com>
2021-10-10 17:37:38 +0400

    feat: Add job menu and list active jobs

--- index.js
+++ index.js
@@ -12,11 +12,25 @@ let grabbed = false
 let newPrice = 0
 let cursor = [1, 1]
 const [TILE_WIDTH, TILE_HEIGHT] = [10, 10]
-const TABS = ['servers', 'developers', 'services']
+const TABS = ['servers', 'developers', 'entertainment', 'services']
 let tab = [TABS[0]]
+
+function animateValue(obj, start, end, duration) {
+  let startTimestamp = null;
+  const step = (timestamp) => {
+    if (!startTimestamp) startTimestamp = timestamp;
+    const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+    obj.innerHTML = Math.floor(progress * (end - start) + start);
+    if (progress < 1) {
+      window.requestAnimationFrame(step);
+    }
+  };
+  window.requestAnimationFrame(step);
+}
 
 // when game loads
 localStorage.setItem('balance', 122000)
+localStorage.setItem('developers', JSON.stringify({}))
 document.getElementById('balance-amount').innerText = 122000
 let balance = Number(localStorage.getItem('balance'))
 
@@ -31,6 +45,13 @@ let balance = Number(localStorage.getItem('balance'))
 //   [[2, 4], [4, 5]]
 // ]
 
+const getter = function (initialState, key) {
+  let temp = initialState
+  const previous = JSON.parse(localStorage.getItem(key))
+  if (previous) temp = previous
+  return temp
+}
+
 class Product {
   constructor(id, name, icon, price, cursor) {
     this.id = id
@@ -38,6 +59,7 @@ class Product {
     this.icon = icon
     this.price = price
     this.cursor = cursor
+    this.type = 'product'
   }
 }
 
@@ -49,8 +71,37 @@ class Developer {
     this.price = price
     this.cursor = cursor
     this.skills = skills
+    this.type = 'developer'
   }
 }
+
+class Job {
+  constructor(id, title, reward, company, duration, requirements) {
+    this.id = id;
+    this.title = title;
+    this.reward = reward
+    this.company = company
+    this.duration = duration
+    this.requirements = requirements
+    this.type = 'job'
+  }
+}
+
+class Requirement {
+  constructor(title, icon, prefix, palette) {
+    this.title = title
+    this.icon = icon
+    this.prefix = prefix
+    this.palette = palette
+    this.type = 'requirement'
+  }
+}
+
+const jobs = [
+  new Job(0, 'Coffee review Website', 10000, 'frestea', 0.2, [new Requirement('Python', '', 'PY', ["#173248", "white"]), new Requirement('Javascript', '', 'JS', ["#EAD41B", "#000000"])]),
+  new Job(1, 'Ecommerce Website', 45000, 'econovu', 2, [new Requirement('Javascript', '', 'JS', ["#EAD41B", "#000000"])]),
+  new Job(2, 'Alt coin trading platform', 75000, 'traderex', 5, [new Requirement('Scala', '', 'Scala', ["#D12F2D", "#000000"]), new Requirement('Java', '', 'Java', ["#E97B18", "#4A738E"])])
+]
 
 let products = [];
 
@@ -114,6 +165,8 @@ const detectObject = function (coordX, coordY) {
 document.addEventListener('mousemove', (event) => {
   clearRect()
 
+  const selected = JSON.parse(localStorage.getItem('selected'))
+
   const map = document.getElementById('map').getBoundingClientRect();
   const [mouseX, mouseY] = [event.clientX, event.clientY]
   const { left, right, top, bottom } = map;
@@ -128,6 +181,10 @@ document.addEventListener('mousemove', (event) => {
       ctx.strokeStyle = "white";
       ctx.lineWidth = 3;
       ctx.strokeRect(coordX * tileSize, coordY * tileSize, 64 * cursor[0], 64 * cursor[1]);
+
+      if (grabbed) {
+        ctx.drawImage(inventory[selected], coordX * tileSize, coordY * tileSize, 64 * cursor[0], 64 * cursor[1])
+      }
     }
 
   }
@@ -161,7 +218,30 @@ desk3.src = "./desk3.png";
 const desk4 = document.createElement('img');
 desk4.src = "./desk4.png";
 const desk5 = document.createElement('img');
-desk5.c = "./desk5.png";
+desk5.src = "./desk5.png";
+const cafeImage = document.createElement('img');
+cafeImage.src = "./cafe.png";
+const bordRoomImage = document.createElement('img');
+bordRoomImage.src = './boardroom.png'
+
+const desk1Off = document.createElement('img');
+desk1Off.src = "./desk1-off.png";
+const desk2Off = document.createElement('img');
+desk2Off.src = "./desk2-off.png";
+const desk3Off = document.createElement('img');
+desk3Off.src = "./desk3-off.png";
+const desk4Off = document.createElement('img');
+desk4Off.src = "./desk4-off.png";
+const desk5Off = document.createElement('img');
+desk5Off.src = "./desk5-off.png";
+
+const disabledInventory = {
+  3: desk1Off,
+  4: desk2Off,
+  5: desk3Off,
+  6: desk4Off,
+  7: desk5Off,
+}
 
 const inventory = {
   0: amazonServer,
@@ -172,7 +252,22 @@ const inventory = {
   5: desk3,
   6: desk4,
   7: desk5,
+  8: cafeImage,
+  9: bordRoomImage
 }
+
+const product0 = new Product(0, "RDS", amazonServer, 1299, [1, 1])
+const product1 = new Product(1, "Azure DB", azureServer, 2999, [1, 1])
+const product2 = new Product(2, "Firebase", firebaseServer, 599, [1, 1])
+const product3 = new Developer(3, "John", developer1, 30000, ['Python', 'Javascript'], [3, 2])
+const product4 = new Developer(4, "Isabella", developer2, 15000, ['SQL', 'ReactJS'], [3, 2])
+const product5 = new Developer(5, "Andrew", developer3, 20000, ['C++', 'ASP'], [3, 2])
+const product6 = new Developer(6, "Bobby", developer4, 75000, ['Scala', 'Java'], [3, 2])
+const product7 = new Developer(7, "Jade", developer5, 56000, ['AWS'], [3, 2])
+const product8 = new Product(8, "Cafe", cafeImage, 45000, [3, 3])
+const product9 = new Product(9, "Board Room", bordRoomImage, 25000, [3, 2])
+
+const prods = [product0, product1, product2, product3, product4, product5, product6, product7, product8, product9]
 
 document.getElementById('map').addEventListener('click', function (event) {
   if (shopActive) return
@@ -201,6 +296,22 @@ document.getElementById('map').addEventListener('click', function (event) {
       layout.push([[coordX, coordX + cursor[0] - 1], [coordY, coordY + cursor[1] - 1]])
       localStorage.setItem('layout', JSON.stringify(layout))
 
+      if (prods[storageSelected].type === 'developer') {
+        let developers = getter({}, 'developers')
+
+        developers[prods[storageSelected].id] = {
+          id: prods[storageSelected].id,
+          skills: prods[storageSelected].skills,
+          active: false,
+          jobId: null,
+          tile: [coordX, coordY],
+          cursor,
+          image: storageSelected
+        }
+
+        localStorage.setItem('developers', JSON.stringify(developers))
+      }
+
       localStorage.removeItem('selected')
     }
     grabbed = false
@@ -216,17 +327,192 @@ products = {
     new Product(2, "Firebase", firebaseServer, 599, [1, 1]),
   ],
   'developers': [
-    new Developer(3, "John", developer1, 30000, ['Python', 'JS'], [3, 2]),
+    new Developer(3, "John", developer1, 30000, ['Python', 'Javascript'], [3, 2]),
     new Developer(4, "Isabella", developer2, 15000, ['SQL', 'ReactJS'], [3, 2]),
     new Developer(5, "Andrew", developer3, 20000, ['C++', 'ASP'], [3, 2]),
     new Developer(6, "Bobby", developer4, 75000, ['Scala', 'Java'], [3, 2]),
     new Developer(7, "Jade", developer5, 56000, ['AWS'], [3, 2]),
+  ],
+  'entertainment': [
+    new Product(8, "Cafe", cafeImage, 45000, [3, 3]),
+    new Product(9, "Board Room", bordRoomImage, 25000, [3, 2]),
   ],
   'services': [
 
   ]
 }
 
+const checkJobAvailability = function (work) {
+  let developers = getter({}, 'developers')
+  let storageJobs = getter([], 'jobs')
+
+  const requirements = work.requirements
+
+  let pass = false
+  requirements.forEach((requirement, count) => {
+    Object.keys(developers).forEach((developer) => {
+      if (developers[developer].skills.includes(requirement.title) && !developers[developer].active && count == requirements.length - 1) pass = true
+    })
+  })
+
+  if (storageJobs[work.id]) {
+    if (storageJobs[work.id].ended) {
+      return false
+    }
+    return true
+  } else if (!pass) {
+    return true
+  } else {
+    return false
+  }
+}
+
+const makeWorkerBusy = function (requirements, jobId) {
+  let developers = getter({}, 'developers')
+
+  let pass = false
+  let selected = null
+  requirements.forEach((requirement, count) => {
+    Object.keys(developers).forEach((developer) => {
+      if (developers[developer].skills.includes(requirement.title) && count == requirements.length - 1) {
+        pass = true
+        selected = developer
+      }
+    })
+  })
+
+  if (pass) {
+    objectsCtx.clearRect(developers[selected].tile[0] * 64, developers[selected].tile[1] * 64, 64 * developers[selected].cursor[0], 64 * developers[selected].cursor[1])
+    objectsCtx.drawImage(disabledInventory[developers[selected].image], developers[selected].tile[0] * 64, developers[selected].tile[1] * 64, 64 * developers[selected].cursor[0], 64 * developers[selected].cursor[1])
+    developers[selected] = { ...developers[selected], active: true, jobId }
+    localStorage.setItem('developers', JSON.stringify(developers))
+  } else {
+    return false
+  }
+}
+
+const makeWorkerFree = function (jobId) {
+  let developers = getter({}, 'developers')
+  let selected = null
+
+  Object.keys(developers).forEach((developer) => {
+    if (developers[developer].jobId === Number(jobId)) {
+      selected = developer
+    }
+  })
+
+  if (!selected) return
+  objectsCtx.clearRect(developers[selected].tile[0] * 64, developers[selected].tile[1] * 64, 64 * developers[selected].cursor[0], 64 * developers[selected].cursor[1])
+  objectsCtx.drawImage(inventory[developers[selected].image], developers[selected].tile[0] * 64, developers[selected].tile[1] * 64, 64 * developers[selected].cursor[0], 64 * developers[selected].cursor[1])
+
+  developers[selected] = { ...developers[selected], active: false, jobId: null }
+  localStorage.setItem('developers', JSON.stringify(developers))
+}
+
+const renderJobs = function () {
+  const job = document.getElementById('job');
+  job.innerHTML = null
+
+  let storageJobs = getter({}, 'jobs')
+
+  jobs.forEach((work) => {
+    const item = document.createElement('div')
+    item.id = 'job-item'
+    item.style.position = 'relative'
+
+    // const locked = document.createElement('div')
+    // locked.className = 'locked'
+    // locked.innerText = 'Locked'
+
+    // item.append(locked)
+
+    const title = document.createElement('p')
+    title.innerText = work.title
+    title.id = 'job-item-title'
+
+    const reward = document.createElement('p')
+    reward.innerText = '$ ' + work.reward
+
+    const company = document.createElement('company')
+    company.innerText = 'By ' + work.company
+    company.id = 'job-item-company'
+
+    const flow = document.createElement('div')
+    const duration = document.createElement('p')
+    const clock = document.createElement('i')
+    clock.className = 'fa fa-clock-o'
+    clock.style.color = 'rgb(94, 94, 94)'
+    flow.style.display = 'flex'
+    flow.style.columnGap = '5px'
+    duration.innerText = work.duration + ' Minutes'
+    duration.style.color = 'rgb(94, 94, 94)'
+    duration.style.fontWeight = 'bold'
+    flow.append(clock)
+    flow.append(duration)
+
+    const requirements = document.createElement('div')
+    requirements.className = 'job-item-requirements'
+    requirements.style.marginBottom = '8px'
+    work.requirements.forEach((requirement) => {
+      const requirementItem = document.createElement('div')
+      requirementItem.style.backgroundColor = requirement.palette[0]
+      requirementItem.style.color = requirement.palette[1]
+      requirementItem.className = 'job-item-requirements-item'
+      requirementItem.innerText = requirement.prefix
+      requirements.append(requirementItem)
+    })
+
+    const accept = document.createElement('button')
+    accept.innerText = storageJobs[work.id] && storageJobs[work.id].ended ? 'Claim reward' : "Accept job"
+    accept.setAttribute('status', storageJobs[work.id] && storageJobs[work.id].ended ? 'claim' : 'open')
+    accept.disabled = checkJobAvailability(work);
+    accept.onclick = function () {
+      if (storageJobs[work.id] && storageJobs[work.id].ended) {
+        claim(storageJobs, work.id, work.reward)
+
+        renderJobs()
+      } else {
+        storageJobs[work.id] = {
+          title: work.title,
+          reward: work.reward,
+          requirements: work.requirements,
+          finish: moment().add(work.duration, 'minute'),
+          duration: work.duration,
+          ended: false
+        }
+
+        localStorage.setItem('jobs', JSON.stringify(storageJobs))
+
+        const jobMenu = document.getElementById('job-menu');
+        jobMenu.hidden = true
+      }
+
+      refreshJobs()
+
+      makeWorkerBusy(work.requirements, work.id)
+      checkJobAvailability(work)
+    }
+
+    const requirementTitle = document.createElement('p')
+    requirementTitle.innerText = 'Requirements'
+    requirementTitle.style.fontSize = '14px'
+    requirementTitle.style.fontWeight = 'bold'
+    requirementTitle.style.margin = '5px 0px'
+    requirementTitle.style.paddingTop = '8px'
+    requirementTitle.style.color = 'rgba(0, 0, 0, 0.8)'
+    requirementTitle.style.borderTop = '1px solid rgba(0, 0, 0, 0.15)'
+
+    item.append(title)
+    item.append(reward)
+    item.append(company)
+    item.append(flow)
+    item.append(requirementTitle)
+    item.append(requirements)
+    item.append(accept)
+
+    job.append(item)
+  })
+}
 
 const renderProducts = function (tab, balance) {
   const shop = document.getElementById('shop');
@@ -238,7 +524,7 @@ const renderProducts = function (tab, balance) {
 
     const buy = document.createElement('button')
     buy.id = 'item-buy'
-    buy.innerText = 'BUY'
+    buy.innerText = tab === 'developers' ? 'HIRE' : 'BUY'
 
     if (product.price > balance) {
       buy.disabled = true
@@ -280,9 +566,13 @@ const renderProducts = function (tab, balance) {
       content.appendChild(skills)
     }
 
+    const imageContainer = document.createElement('div');
+    imageContainer.appendChild(product.icon);
+    product.icon.style.maxWidth = '100%'
+
     item.className = "item"
     item.id = product.id
-    item.appendChild(product.icon)
+    item.appendChild(imageContainer)
     item.appendChild(content)
     item.appendChild(buy)
     buy.onclick = function () {
@@ -309,9 +599,7 @@ const renderProducts = function (tab, balance) {
   })
 }
 
-
-
-document.getElementById('content-shop').addEventListener('click', () => {
+document.getElementById('content-shop').parentElement.addEventListener('click', () => {
   balance = Number(localStorage.getItem('balance'))
   shopActive = true
 
@@ -323,6 +611,18 @@ document.getElementById('content-shop').addEventListener('click', () => {
 
   renderProducts(tab, balance)
 })
+
+document.getElementById('content-job').parentElement.addEventListener('click', () => {
+  const jobMenu = document.getElementById('job-menu');
+  jobMenu.hidden = false
+
+  renderJobs()
+})
+
+function handleJobClose() {
+  const jobMenu = document.getElementById('job-menu');
+  jobMenu.hidden = true
+}
 
 function handleShopClose() {
   shopActive = false
@@ -336,14 +636,130 @@ function handleShopClose() {
 
 function setTab(newTab) {
   tab = newTab
-  renderProducts(tab, balance)
 
   TABS.forEach((tab) => {
-    console.log(tab, newTab)
     if (tab === newTab) {
       document.getElementById(`item-${tab}`).setAttribute('active', true)
+      renderProducts(tab, balance)
     } else {
       document.getElementById(`item-${tab}`).setAttribute('active', false)
     }
   })
 }
+
+const endWork = function (workId) {
+  const jobs = getter({}, 'jobs')
+  jobs[workId] = { ...jobs[workId], ended: true }
+  localStorage.setItem('jobs', JSON.stringify(jobs))
+}
+
+const claim = function (storageJobs, job, reward) {
+  const balance = localStorage.getItem('balance')
+  const newBalance = Number(balance) + Number(reward)
+  localStorage.setItem('balance', newBalance)
+
+  const obj = document.getElementById('balance-amount');
+
+  delete storageJobs[job]
+  localStorage.setItem('jobs', JSON.stringify(storageJobs))
+
+  jobs.splice(job, 1)
+
+  animateValue(obj, Number(balance), Number(newBalance), 1000)
+}
+
+const renderProgressbar = function (final, current, reward, job) {
+  let storageJobs = getter({}, 'jobs')
+
+  const container = document.createElement('div')
+  container.className = 'progressbar-container'
+
+  const bar = document.createElement('div')
+  bar.className = 'progressbar'
+  bar.setAttribute('completed', false)
+
+  if (current >= 0) {
+    makeWorkerFree(job)
+    endWork(job)
+    renderJobs()
+
+    bar.style.backgroundColor = 'green'
+    bar.style.cursor = 'pointer'
+    bar.onclick = function (event) {
+      event.preventDefault()
+      claim(storageJobs, job, reward)
+    }
+    bar.setAttribute('completed', true)
+    bar.innerText = 'Claim cash'
+  } else {
+    bar.style.width = `${100 - ((100 / final) * Math.abs(current))}%`
+    bar.style.backgroundColor = '#6d0cce'
+  }
+
+  container.append(bar)
+
+  return container
+}
+
+const refreshJobs = function () {
+  let storageJobs = {}
+  const previousJobs = JSON.parse(localStorage.getItem('jobs'))
+  if (previousJobs) storageJobs = previousJobs
+
+  const empty = document.createElement('p')
+  empty.style.color = 'rgba(0,0,0,0.8)'
+  empty.style.margin = 0;
+  empty.style.padding = '10px 20px';
+  empty.innerText = 'No active job'
+
+  const dashboardContent = document.getElementById('dashboard-content')
+  dashboardContent.innerHTML = null
+
+  if (Object.keys(storageJobs).length === 0) {
+    dashboardContent.append(empty)
+  } else {
+    Object.keys(storageJobs).forEach((job) => {
+      const timeDiff = moment().diff(storageJobs[job].finish, 'second')
+
+      const jobProfile = document.createElement('div')
+      jobProfile.className = 'job-profile'
+
+      const dashboardInformationContent = document.createElement('div')
+      dashboardInformationContent.className = 'dashboardInformationContent'
+
+      const jobTitle = document.createElement('p')
+      jobTitle.innerText = storageJobs[job].title
+      jobTitle.className = 'jobTitle'
+
+      const progress = renderProgressbar(storageJobs[job].duration * 60, timeDiff, storageJobs[job].reward, job)
+
+      dashboardInformationContent.append(jobTitle)
+
+      const requirements = document.createElement('div')
+      requirements.className = 'job-item-requirements'
+      requirements.style.marginBottom = '8px'
+      storageJobs[job].requirements.forEach((requirement) => {
+        const requirementItem = document.createElement('div')
+        requirementItem.style.backgroundColor = requirement.palette[0]
+        requirementItem.style.color = requirement.palette[1]
+        requirementItem.className = 'job-item-requirements-item'
+        requirementItem.innerText = requirement.prefix
+        requirements.append(requirementItem)
+      })
+
+      dashboardInformationContent.append(requirements)
+      dashboardInformationContent.append(progress)
+
+      jobProfile.append(dashboardInformationContent)
+
+      dashboardContent.append(jobProfile)
+
+    })
+  }
+}
+
+refreshJobs()
+
+const x = setInterval(() => {
+  refreshJobs()
+}, 500);
